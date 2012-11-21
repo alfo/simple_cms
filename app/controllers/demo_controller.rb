a54562e501b6b9ab2196849c88c5d9030dea77d0
Alex Forey <me@alexforey.com>
2012-11-21 14:24:44 +0000

    Model Users

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,9 +1,15 @@
 class DemoController < ApplicationController
   def index
-  	render('hello')
+
   end
 
   def hello
-  	render(:text => 'Hello!!!')
+  	@array = [1,2,3,4,5]
+    @id = params[:id].to_i
+    @page = params[:page].to_i
+  end
+
+  def other_hello
+  	redirect_to('hello')
   end
 end
