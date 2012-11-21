0dc1de2fb81b96a7b93efcbb0020bbff5011e3ef
Alex Forey <me@alexforey.com>
2012-11-21 21:37:21 +0000

    Show and List actions

--- app/controllers/demo_controller.rb
+++ app/controllers/demo_controller.rb
@@ -1,15 +1,18 @@
 class DemoController < ApplicationController
-  def index
 
+  def index
+    # render('hello')
+    #redirect_to(:action => 'other_hello')
   end
 
   def hello
-  	@array = [1,2,3,4,5]
+    @array = [1,2,3,4,5]
     @id = params[:id].to_i
     @page = params[:page].to_i
   end
-
+  
   def other_hello
-  	redirect_to('hello')
+    render(:text => 'Hello Everyone!')
   end
+
 end
