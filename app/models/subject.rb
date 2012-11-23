d27b59bfc8f40f2597e61197eb1ea6f9a8dbf1e9
Alex Forey <me@alexforey.com>
2012-11-23 17:51:52 +0000

    Add and edit actions

--- app/models/subject.rb
+++ app/models/subject.rb
@@ -2,4 +2,5 @@ class Subject < ActiveRecord::Base
   scope :visible, where(:visible => true)
   scope :invisible, where(:visible => false)
   scope :search, lambda {|query| where(["name LIKE ?", "%#{query}%"])}
+  attr_accessible :name, :position, :visible
 end
